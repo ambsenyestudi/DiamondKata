4b7a1f4369af00d4e1c339ce0627e740eac63dcb
ambsenyestudi <adrifre7@hotmail.com>
2022-09-25 20:42:09 +0200

    Mirroring service red

---
+++ test/MirroringServiceShould.cs
@@ -0,0 +1,15 @@
+﻿using Diamonds.Domain;
+
+namespace Diamonds.Test;
+
+public class MirroringServiceShould
+{
+    private readonly MirroringService _mirroringService = new MirroringService();
+
+    [Fact]
+    public void Mirror()
+    {
+        var expected = "   A   ";
+        Assert.Equal(expected, _mirroringService.Mirror("   A"));
+    }
+}
