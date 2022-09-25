44715b755d4d435a47dc0e259c0150bf663653cc
ambsenyestudi <adrifre7@hotmail.com>
2022-09-25 19:41:24 +0200

    Letter repo should get up to refactor

--- src/domain/LetterRepository.cs
+++ src/domain/LetterRepository.cs
@@ -4,9 +4,12 @@
     {
         private readonly List<string> _letters = Enumerable
             .Range('A', ('Z' - 'A' + 1))
-            .Select(i => (char)i+"").ToList();
+            .Select(i => (char)i + "").ToList();
 
         public List<string> GetUpTo(string letter) =>
-            _letters.Take(_letters.IndexOf(letter) + 1).ToList();
+            _letters.Take(GetLetterCount(letter)).ToList();
+
+        private int GetLetterCount(string letter) =>
+            _letters.IndexOf(letter) + 1;
     }
 }
--- test/LetterRepositoryShould.cs
+++ test/LetterRepositoryShould.cs
@@ -5,9 +5,12 @@ namespace Diamonds.Test;
 public class LetterRepositoryShould
 {
     private readonly LetterRepository _letterRepository = new();
-    [Fact]
-    public void GetLettersUpTo()
+
+    [Theory]
+    [InlineData("A", "A")]
+    [InlineData("A,B,C,D", "D")]
+    public void GetLettersUpTo(string expected, string input)
     {
-        Assert.Equal("A", string.Join(",", _letterRepository.GetUpTo("A")));
+        Assert.Equal(expected, string.Join(",", _letterRepository.GetUpTo(input)));
     }
 }
