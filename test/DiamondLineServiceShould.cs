c48c04c2b0d60a2e10d94ba0084076fe2933c3f5
ambsenyestudi <adrifre7@hotmail.com>
2022-09-26 09:27:49 +0200

    Diamond line services should create lines red

---
+++ test/DiamondLineServiceShould.cs
@@ -0,0 +1,27 @@
+﻿using Diamonds.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diamonds.Test
+{
+    public class DiamondLineServiceShould
+    {
+        private readonly DiamondLineService  _lineService = new DiamondLineService();
+        [Fact]
+        public void CreateLines()
+        {
+            var expected =new List<string>
+            {
+                "  A\n",
+               " B B\n",
+               " B B\n",
+               "  A\n"
+            };
+            var lines = _lineService.CreateLines('B');
+            Assert.Equal(expected, lines.Select(x => x.ToString()).ToList());
+        }
+    }
+}
