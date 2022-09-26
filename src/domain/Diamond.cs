a4c2b46ae412dba674a1f610a703cb524b8d6c36
ambsenyestudi <adrifre7@hotmail.com>
2022-09-26 09:19:47 +0200

    Extract method

--- src/domain/Diamond.cs
+++ src/domain/Diamond.cs
@@ -11,10 +11,13 @@ public class Diamond
 
     public string Print()
     {
-        var upperLines = _lines.Select(x => x.ToString().TrimEnd());
+        var upperLines = RenderLines();
         var renderedLines = upperLines.Concat(MirrorLines(upperLines));
         return string.Join(LINE_BREAK, renderedLines) + LINE_BREAK;
     }
+
+    private IEnumerable<string> RenderLines() =>
+        _lines.Select(x => x.ToString().TrimEnd());
 
     private static IEnumerable<string> MirrorLines(IEnumerable<string> renderedLines, int offset = 1) =>
         renderedLines.Reverse().Skip(offset);
