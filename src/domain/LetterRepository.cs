2652f18054e030af326c04d2dc9bb079e925931d
ambsenyestudi <adrifre7@hotmail.com>
2022-09-26 09:43:54 +0200

    Feature completed

--- src/domain/LetterRepository.cs
+++ src/domain/LetterRepository.cs
@@ -2,8 +2,11 @@
 
 public class LetterRepository : ILetterRepository
 {
-    public List<char> GetUpTo(char lastLetter)
-    {
-        throw new NotImplementedException();
-    }
+    private const int ALPHABETH_COUNT = 52;
+    private readonly List<char> _letters = Enumerable.Range('A', ALPHABETH_COUNT).Select(i => (char)i).ToList();
+    public List<char> GetUpTo(char lastLetter) =>
+            _letters.Take(GetLetterCount(lastLetter)).ToList();
+
+    private int GetLetterCount(char letter) =>
+        _letters.IndexOf(letter) + 1;
 }
